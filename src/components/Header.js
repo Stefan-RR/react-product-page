9e05e01f852a2530c3f765a60b02d12661c00329
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-06 13:54:01 -0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header () {
+    return (
+        <div className="Header">
+            <a>Home</a>
+            <a>Shop</a>
+            <a>About</a>
+            <a>Contact</a>
+        </div>
+    )
+}
+
+export default Header
