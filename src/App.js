3169a2563b0cd6efbd3fd995ef2caf18b8ce3352
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-06 13:54:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Header from './components/Header'
+import Container from './components/Container'
 import Description from './components/Description'
 import Image from './components/Image'
+import watch1 from './assets/watch1.jpeg'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Image />
-      <Description title={"Watch One"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
+      <Container >
+        <Image source={watch1} name={"main-pic"}/>
+        <Description name={"main-description"} title={"Watch One"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
+      </Container>
     </div>
   );
 }
