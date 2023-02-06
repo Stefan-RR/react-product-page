9a098d66c6eee6ae0aa7e8d392b79c4f4870a193
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-05 22:38:10 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Card from './components/Card'
+import Header from './components/Header'
+import Description from './components/Description'
+import Image from './components/Image'
+
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Header />
+      <Image />
+      <Description title={"Watch One"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}/>
     </div>
   );
 }
